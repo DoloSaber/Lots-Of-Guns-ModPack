71fcd93e14a14519c74ca919ba31f353df292806
DoloC8 <754457925@qq.com>
2025-01-07 20:32:50 +0800

    Add files via upload

---
+++ kubejs/server_scripts/MobCoin.js
@@ -0,0 +1,18 @@
+//生物死亡事件
+EntityEvents.death( event => {
+    //获取实际击杀源  tacz.bullet
+    let player = event.source.actual
+    if(player.isPlayer()){   
+        let mobType = event.entity.getType().toString
+        let mobCoin = parseInt(global.$MobCoinImpl.get(mobType))
+        if (mobCoin || 0 ) {
+
+            player.tell("Mob: " + parseInt(mobCoin))
+            let coin = new global.PlayerCoin(player);
+            coin.add(mobCoin);
+            player.tell("coin: " + coin.get())
+
+        }
+    }
+
+})
